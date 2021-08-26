9e127ab8d0f9d9379f6f6b5583e7316246f54f8a
atillaturkmen <turkmenatilla522@gmail.com>
2021-08-26 21:31:28 +0300

    added high scores fragment

--- app/src/main/java/com/tayyar/pianotiles/HighScoresFragment.kt
+++ app/src/main/java/com/tayyar/pianotiles/HighScoresFragment.kt
@@ -1,10 +1,13 @@
 package com.tayyar.pianotiles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.tayyar.pianotiles.databinding.FragmentHighScoresBinding
 
 /** Shows high scores */
 class HighScoresFragment : Fragment() {
@@ -12,8 +15,24 @@ class HighScoresFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_high_scores, container, false)
+    ): View {
+        val binding: FragmentHighScoresBinding =
+            FragmentHighScoresBinding.inflate(inflater, container, false)
+
+        // get high scores and show in table
+        val sharedPref = activity?.getSharedPreferences(
+            getString(R.string.shared_preferences_name),
+            AppCompatActivity.MODE_PRIVATE
+        )
+        val highScores = sharedPref?.all?.toSortedMap(compareBy<String> { it.toInt() })
+
+        for (score in highScores!!.iterator()) {
+            val item = inflater.inflate(R.layout.list_item, binding.highScoresTable, false)
+            item.findViewById<TextView>(R.id.speed).text = score.key
+            item.findViewById<TextView>(R.id.score).text = score.value.toString()
+            binding.highScoresTable.addView(item)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/tayyar/pianotiles/MainActivity.kt
+++ app/src/main/java/com/tayyar/pianotiles/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tayyar.pianotiles
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -27,21 +26,13 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(binding.navView, navController)
 
         // menu can only be opened in main menu
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
             if (destination.id == controller.graph.startDestination) {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
             } else {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             }
         }
-
-        // get high scores and show them as a toast
-        val sharedPref = getSharedPreferences(
-            getString(R.string.shared_preferences_name),
-            MODE_PRIVATE
-        ) ?: return
-        val highScore = sharedPref.all.toSortedMap()
-        Toast.makeText(this, "High Scores: \n$highScore", Toast.LENGTH_LONG).show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
